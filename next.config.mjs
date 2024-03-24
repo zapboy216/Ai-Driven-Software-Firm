5989d86ea376a131b52f20ee9a7edbc482b4dadb
zapboy216 <zapboy216@gmail.com>
2024-03-24 08:20:00 -0400

    Update Sentry configuration

--- next.config.mjs
+++ next.config.mjs
@@ -43,8 +43,8 @@ export default withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
     // FIXME: Add your Sentry organization and project names
-    org: 'nextjs-boilerplate-org',
-    project: 'nextjs-boilerplate',
+    org: 'developerpentester',
+    project: 'javascript-nextjs-c9',
   },
   {
     // For all available options, see:
