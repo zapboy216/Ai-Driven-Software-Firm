74a9f1355e2b3ead60ae8a8ab042ecb05e747d11
zapboy216 <zapboy216@gmail.com>
2024-04-29 09:55:48 -0400

    replaced navbar

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -1,7 +1,7 @@
 // BaseTemplate.tsx
 import { useTranslations } from 'next-intl';
 
-import { AppConfig } from '@/utils/AppConfig';
+import { Navbar } from '@/components/component/navbar';
 
 const BaseTemplate = (props: {
   leftNav: React.ReactNode;
@@ -11,34 +11,11 @@ const BaseTemplate = (props: {
   const t = useTranslations('BaseTemplate');
 
   return (
-    <div className="w-full px-20 py-10 text-gray-700 antialiased">
-      <div className="mx-auto max-w-screen-md lg:max-w-full">
-        <header className="border-b border-gray-300 px-10">
-          <div className="pt-8">
-            <h1 className="text-3xl font-bold text-gray-900">
-              {AppConfig.name}
-            </h1>
-            <h2 className="text-xl">{t('description')}</h2>
-          </div>
+    <>
+      <Navbar />
 
-          <div className="flex justify-between px-10">
-            <nav>
-              <ul className="flex flex-wrap gap-x-3 text-xl">
-                {props.leftNav}
-              </ul>
-            </nav>
-
-            <nav>
-              <ul className="flex flex-wrap gap-x-3 text-xl">
-                {props.rightNav}
-              </ul>
-            </nav>
-          </div>
-        </header>
-
-        <main>{props.children}</main>
-      </div>
-    </div>
+      <main>{props.children}</main>
+    </>
   );
 };
 
